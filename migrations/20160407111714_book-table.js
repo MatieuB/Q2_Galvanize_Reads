ea200a79d735bab5e1532c40ce97b4f28ff02448
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-04-07 11:28:41 -0600

    landing page layout,db schemas

---
+++ migrations/20160407111714_book-table.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('books', function (table) {
+    table.increments();
+    table.string('title');
+    table.text('description');
+    table.string('cover-url');
+    table.integer('author_id').references('authors.id');
+  });
+
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('books');
+
+};
---
+++ migrations/20160407112349_create-genres.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('genres', function (table) {
+    table.increments();
+    table.string('genre');
+
+});
+}
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('genres');
+
+};
