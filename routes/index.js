ebb608b14b8ce9c8d631c52db8548825702388a4
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-04-07 15:10:45 -0600

    books layout mostly

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('knex')(require('../knexfile')['development']);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Galvanize Reads' });
+});
+router.get('/books', function(req, res, next) {
+    knex('authors_books')
+        .innerJoin('authors','authors_books.author_id','authors.id')
+        .innerJoin('books','authors_books.books_id','books.id')
+        .innerJoin('genres','books.genre_id','genres.id')
+        .then(function(bookData){
+            console.log(bookData);
+            res.render('books', { title: 'Galvanize Reads',
+        book:bookData });
+        })
 });
 
 module.exports = router;
