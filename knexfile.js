bb60d7a7bdae8561d780735801fd3fe45f1c41c4
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-04-08 19:01:08 -0600

    bugfix knexfile

--- knexfile.js
+++ knexfile.js
@@ -8,17 +8,17 @@ module.exports = {
        pool: {
          min: 2,
          max: 10
-       },
+        }
+    },
     production: {
        client: 'postgresql',
        connection: process.env.DATABASE_URL,
        pool: {
          min: 2,
          max: 10
-       },
+        }
+    },
        seeds: {
         directory: './seeds/'
        }
-    }
-
-}
+   };
